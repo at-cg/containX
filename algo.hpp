6fd6da105acef474eaa9ea5c6cf1ea67a37c5fd0
Chirag Jain <cjain7@gatech.edu>
2021-12-13 23:03:54 -0800

    WIP: minimizer computation

--- algo.hpp
+++ algo.hpp
@@ -11,6 +11,7 @@ struct algoParams
   uint32_t k;                     //k-mer length (<=16)
   float d;                        //[0,1] sampling density
   float cutoff;                   //what fraction of minimizers must match for redundancy
+  bool hpc;                       //parse k-mers in homopolymer compressed space
 };
 
 /**
@@ -24,8 +25,80 @@ void computeMinimizersFromString(std::vector<uint32_t> &container, const std::st
 {
   assert (param.k >= 10 && param.k <= 16);
   assert (param.d > 0 && param.d <= 1.0);
+  assert (beg >= 0 && beg < str.length());
+  assert (end >= beg && end <= str.length());
 
-  //TODO: complete this function
+  if (end == beg)
+    return;
+
+  //'U' is for integer literal of type unsigned int
+  uint32_t shift1 = 2*(param.k - 1), mask = (1U<<2*param.k) - 1, kmer = 0;
+  float hashkmer;
+
+  for (uint32_t i = beg, l = 0; i < end; ++i) {
+    int c = seq_nt4_table[(uint8_t)str[i]];
+    if (c < 4) { // not an ambiguous base
+      if (param.hpc) {
+        int skip_len = 1;
+        if (i + 1 < end && seq_nt4_table[(uint8_t)str[i + 1]] == c) {
+          for (skip_len = 2; i + skip_len < end; ++skip_len)
+            if (seq_nt4_table[(uint8_t)str[i + skip_len]] != c)
+              break;
+          i += skip_len - 1;
+        }
+      }
+      if (rev == false)
+        kmer = (kmer << 2 | c) & mask;           // forward k-mer
+      else
+        kmer = (kmer >> 2) | (3U^c) << shift1;   // reverse k-mer
+      l++;
+
+      if (l >= k)
+      {
+        hashkmer = hash32(kmer, mask) * 1.0 / UINT32_MAX;
+        if (hashkmer < d) container.emplace_back(kmer);
+      }
+    }
+    else {
+      l = 0;
+    }
+  }
+}
+
+/**
+ * @param[in] src_vertex  starting vertex
+ * @param[in] start_pos   0-based, leftmost position in read string
+ */
+void dfs_procedure (graphcontainer &g, uint32_t src_vertex, uint32_t start_pos, uint32_t remaining_depth_bases, std::set<uint32_t> &visited_vertices, std::vector<uint32_t> &minimizers, const algoParams &param)
+{
+  if (remaining_depth_bases == 0) return;
+  if (visited_vertices.find(src_vertex) != visited_vertices.end()) return;
+  assert (src_vertex < g.vertexCount);
+
+  bool orientation = src_vertex & 1;
+  uint32_t src_readid = src_vertex >> 1;
+  uint32_t beg = start_pos, end;
+  if (remaining_depth_bases > g.readseq[src_readid].length())
+  {
+    end = g.readseq[src_readid].length();
+    remaining_depth_bases = remaining_depth_bases - g.readseq[src_readid].length();
+  }
+  else
+  {
+    end = remaining_depth_bases;
+    remaining_depth_bases = 0;
+  }
+
+  assert (end <= g.readseq[src_readid].length());
+  assert (beg < end);
+
+  computeMinimizersFromString(minimizers, g.readseq[src_readid], beg, end, orientation, param);
+  visited_vertices.insert (src_vertex);
+
+  if (remaining_depth_bases > 0)
+    for (uint32_t j = g.offsets[src_vertex]; j < g.offsets[src_vertex+1]; j++)
+      if (g.redundant[j] == false)
+        dfs_procedure (g, j, 0, remaining_depth_bases, visited_vertices, mmWalkRead, param);
 }
 
 void identifyRedundantReads(graphcontainer &g, const algoParams &param)
@@ -42,6 +115,33 @@ void identifyRedundantReads(graphcontainer &g, const algoParams &param)
   {
     if (g.contained[i] == true && g.redundant[i] == false)
     {
+      //confirm if parent reads are still available, otherwise nothing to do
+      {
+        uint32_t available_parent_count = 0;
+        for (uint32_t j = g.containment_offsets[i]; j < g.containment_offsets[i+1]; j++) {
+          if (g.redundant[j] == false)
+            available_parent_count++;
+        }
+        if (available_parent_count == 0)
+          continue;
+      }
+
+      //collect minimizers by starting DFS from contained read
+      {
+        assert (g.readseq[i].length() > 0);
+        uint32_t depth_no_bases = param.depth * g.readseq[i].length();
+        std::set<uint32_t> visited_vertices;
+        for (uint32_t j = g.offsets[i]; j < g.offsets[i+1]; j++)
+          if (g.redundant[j] == false)
+            dfs_procedure (g, j, 0, depth_no_bases, visited_vertices, mmWalkRead, param);
+      }
+
+      //collect minimizers from parent reads
+      {
+
+      }
+
+      //compare minimizer lists and decide
     }
   }
 }
--- main.cpp
+++ main.cpp
@@ -15,16 +15,19 @@ int main(int argc, char *argv[])
   std::string gfadumpfilename;
   bool printReadStrings = true;
   int fuzz = INT32_MAX;
-  bool removeContainedReads = false;
+  bool removeAllContainedReads = false;
+  algoParams param;
+  param.hpc = false;
 
-  while ((c = ketopt(&o, argc, argv, 1, "l:i:d:D:t:c", 0)) >= 0)
+  while ((c = ketopt(&o, argc, argv, 1, "l:i:d:D:t:cH", 0)) >= 0)
   {
     if (c == 'i') min_ovlp_identity = atof(o.arg);
     else if (c == 'l') min_ovlp_len = atoi(o.arg);
     else if (c == 'd') gfadumpfilename = o.arg;
     else if (c == 'D') gfadumpfilename = o.arg, printReadStrings = false;
     else if (c == 't') fuzz = atoi(o.arg);
-    else if (c == 'c') removeContainedReads = true;
+    else if (c == 'c') removeAllContainedReads = true;
+    else if (c == 'H') param.hpc = true;
   }
 
   //print usage
@@ -33,6 +36,7 @@ int main(int argc, char *argv[])
     std::cerr << "Options:\n";
     std::cerr << "  -l NUM      min overlap length, default " << min_ovlp_len << "\n";
     std::cerr << "  -i NUM      min overlap percentage identity [0.0-100.0], default " << min_ovlp_identity << "\n";
+    std::cerr << "  -H          use homopolymer-compressed k-mer\n";
     std::cerr << "  -c          mark all contained reads as redundant and remove\n";
     std::cerr << "  -t NUM      enable transitive reduction using given fuzz, disabled by default\n";
     std::cerr << "  -d FILE     dump graph in gfa format\n";
@@ -45,7 +49,7 @@ int main(int argc, char *argv[])
   assert (fuzz >= 0);
 
   graphcontainer g;
-  ovlgraph_gen (argv[o.ind], argv[o.ind+1], min_ovlp_identity, min_ovlp_len, fuzz, removeContainedReads, g);
+  ovlgraph_gen (argv[o.ind], argv[o.ind+1], min_ovlp_identity, min_ovlp_len, fuzz, removeAllContainedReads, g);
 
   if (!gfadumpfilename.empty())
     g.outputGFA (gfadumpfilename, printReadStrings);
@@ -53,7 +57,6 @@ int main(int argc, char *argv[])
   //printContainmentDegreeDistribution (g);
   //printDegreeDistribution (g);
 
-  algoParams param;
   {
     param.maxContainmentDegree = 5, param.depth = 5, param.k = 16;
     param.d = 1.0/param.k, param.cutoff = 1.0;
